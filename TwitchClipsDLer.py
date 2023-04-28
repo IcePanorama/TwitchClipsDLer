6e744eb47b57768b39f5428f502042eb1682cba2
IcePanorama <87292274+IcePanorama@users.noreply.github.com>
2023-04-28 19:48:36 -0400

    first commit

---
+++ TwitchClipsDLer.py
@@ -0,0 +1,123 @@
+'''
+    Script to make dl'ing twitch clips easier
+    Was written specifically to make dl'ing clips that I've made easier,
+    but idk why it wouldn't work for clips in general tho.
+'''
+import yt_dlp
+from time import sleep
+from os import system, name
+
+def ClearScreen():
+    if name == 'nt':
+        _ = system('cls')
+    else:
+        _ = system('clear')
+
+def DisplayLinks(links):
+    ClearScreen()
+    print("Links:")
+
+    x = 0
+    while x < len(links):
+        item = x + 1
+        print(str(item) + " - " + links[x])
+        x += 1
+
+    print("\n")
+
+def EditLinks(links):
+    entry = ""
+    while True:
+        DisplayLinks(links)
+        entry = input("Enter the # of the entry you'd like to edit or type [X] to continue: ")
+
+        # check that entry exists
+        if entry.upper() == "X":
+            break
+        else:
+            # if entry != a number, throw err
+            try:
+                if int(entry) - 1 >= 0 and int(entry) - 1 < len(links):
+                    while True:
+                        userInput = input("\n[E]dit or [D]elete or [C]ancel?\n> ")
+                    
+                        # edit entry
+                        if userInput.upper() == "E":
+                           links[int(entry) - 1] = input("Enter replacement link for #" + entry + ": ")
+                           break
+                        # delete entry
+                        elif userInput.upper() == "D":
+                            links.pop(int(entry) - 1)
+                            break
+                        # skip entry
+                        elif userInput.upper() != "C":
+                            print("Invalid Input")
+                            sleep(1)
+                            ClearScreen()
+                            DisplayLinks(links)
+                        else:
+                            break
+                else:
+                    print("Invalid Input")
+                    sleep(1)
+            except:    
+                print("Invalid Input")
+                sleep(1)
+
+    return links
+
+def DownloadLinks(links):
+    outputFormat = 'Clips\%(upload_date)s-%(timestamp)s-%(creator)s-%(title)s.%(ext)s'
+    timeout = 600 # wait 10 mins w/o internet before giving up
+    
+    ydl_opts = {
+        'outtmpl': outputFormat,
+        'socket_timeout': timeout
+    }
+
+    x = 0
+    while x < len(links):
+        ClearScreen()
+        print("Downloading clip #" + str(x + 1))
+        
+        try:
+            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                ydl.download([links[x]])
+        except:
+            print("Download error, skipping")
+            sleep(1)
+        x += 1
+
+def Main():
+    links = [];
+
+    # Prompt user for to either enter a twitch link or X to quit
+    lastResponse = "";
+    while lastResponse.upper() != "X":
+        lastResponse = input("Enter clip link or [X] to continue: ")
+       
+        # Prevents blank entries/entries that start with blanks
+        try:
+            if lastResponse[0] != "" and lastResponse[0] != " " and lastResponse.upper() != "X":
+                links.append(lastResponse)
+        except:
+            pass
+
+        if len(links) > 0:
+            DisplayLinks(links)
+
+    if len(links) > 0:
+        # Display list of clip links
+        DisplayLinks(links)
+
+        # prompt user to say they're correct or to enter the link # to replace it
+        userInput = input("[E]dit links or [Press Any Key to Continue]\n> ")
+        if userInput.upper() == "E":
+            links = EditLinks(links)  
+
+        # DL clips as %(upload_date)s-%(timestamp)s-%(creator)s-%(title)s.%(ext)s to Clips folder
+        DownloadLinks(links)
+
+if __name__ == "__main__":
+    ClearScreen()
+    Main()
