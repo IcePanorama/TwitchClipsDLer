6b387e15bb2939efdd50aa46c1d3a6809dfeb9db
IcePanorama <87292274+IcePanorama@users.noreply.github.com>
2023-04-28 20:41:56 -0400

    Created Utilities.py

---
+++ Utilities.py
@@ -0,0 +1,23 @@
+# "A quickly hacked together tool meant to make downloading many Twitch clips at once easier."
+# Copyright (C) 2023  IcePanorama
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+from os import system, name
+
+def ClearScreen():
+    if name == 'nt':
+        _ = system('cls')
+    else:
+        _ = system('clear')
