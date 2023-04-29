1fd22524569e1159d3df0d0830dd7835aadad67f
IcePanorama <87292274+IcePanorama@users.noreply.github.com>
2023-04-28 23:11:32 -0400

    Updated licenses notices

--- src/Utilities.py
+++ src/Utilities.py
@@ -1,7 +1,7 @@
-# TwitchClipsDLer
+# This file is part of TwitchClipsDLer
 # Copyright (C) 2023  IcePanorama
 #
-# This program is free software: you can redistribute it and/or modify
+# TwitchClipsDLer is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
